0aab9a1c4c6cb0453d2ba0d6d1298e8b1dcd889f
Dmitry Atapin <dmitry.atapin.83@gmail.com>
2024-06-20 11:06:41 +0300

    Minor fix
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>

--- test/integration/cmn/client/invoke.go
+++ test/integration/cmn/client/invoke.go
@@ -76,7 +76,7 @@ func NBTxInvokeWithSign(network *nwo.Network, peer *nwo.Peer, orderer *nwo.Order
 }
 
 func invokeTx(network *nwo.Network, peer *nwo.Peer, orderer *nwo.Orderer, userOrg string,
-	channel string, ccName string, args ...string) (txId string) {
+	channel string, ccName string, expectedError string, args ...string) (txId string) {
 	lh := nwo.GetLedgerHeight(network, peer, channel)
 
 	sess, err := network.PeerUserSession(peer, userOrg, commands.ChaincodeInvoke{
@@ -103,12 +103,18 @@ func invokeTx(network *nwo.Network, peer *nwo.Peer, orderer *nwo.Orderer, userOr
 
 	By("creating the deliver client to peer " + peer.ID())
 	pcc := network.PeerClientConn(peer)
-	defer pcc.Close()
+	defer func() {
+		err := pcc.Close()
+		Expect(err).NotTo(HaveOccurred())
+	}()
 	ctx, cancel := context.WithTimeout(context.Background(), network.EventuallyTimeout)
 	defer cancel()
 	dc, err := pb.NewDeliverClient(pcc).Deliver(ctx)
 	Expect(err).NotTo(HaveOccurred())
-	defer dc.CloseSend()
+	defer func() {
+		err := dc.CloseSend()
+		Expect(err).NotTo(HaveOccurred())
+	}()
 
 	By("starting filtered delivery on peer " + peer.ID())
 	deliverEnvelope, err := protoutil.CreateSignedEnvelope(
@@ -205,8 +211,7 @@ func invokeTx(network *nwo.Network, peer *nwo.Peer, orderer *nwo.Orderer, userOr
 					for _, r := range batchResponse.TxResponses {
 						if hex.EncodeToString(r.GetId()) == txId {
 							Expect(isValid).To(BeTrue())
-
-							Expect(r.Error).To(BeNil())
+							checkErrorIfNeeded(r, expectedError)
 							return
 						}
 					}
@@ -216,16 +221,25 @@ func invokeTx(network *nwo.Network, peer *nwo.Peer, orderer *nwo.Orderer, userOr
 	}
 }
 
+func checkErrorIfNeeded(txResponse *pbfound.TxResponse, expectedError string) {
+	if expectedError == "" {
+		Expect(txResponse.Error).To(BeNil())
+	}
+
+	Expect(txResponse.Error).NotTo(BeNil())
+	Expect(txResponse.Error.Error).To(Equal(expectedError))
+}
+
 // TxInvoke func for invoke to foundation fabric
 func TxInvoke(network *nwo.Network, peer *nwo.Peer, orderer *nwo.Orderer,
 	channel string, ccName string, args ...string) (txId string) {
-	return invokeTx(network, peer, orderer, "User1", channel, ccName, args...)
+	return invokeTx(network, peer, orderer, "User1", channel, ccName, "", args...)
 }
 
 // TxInvokeByRobot func for invoke to foundation fabric from robot
 func TxInvokeByRobot(network *nwo.Network, peer *nwo.Peer, orderer *nwo.Orderer,
 	channel string, ccName string, args ...string) (txId string) {
-	return invokeTx(network, peer, orderer, "User2", channel, ccName, args...)
+	return invokeTx(network, peer, orderer, "User2", channel, ccName, "", args...)
 }
 
 // TxInvokeWithSign func for invoke with sign to foundation fabric
@@ -253,151 +267,13 @@ func TxInvokeWithSignErrorReturned(
 	nonce string,
 	errMsg string,
 	args ...string,
-) {
+) (txId string) {
 	ctorArgs := append(append([]string{fn, requestID, channel, ccName}, args...), nonce)
 	pubKey, sMsg, err := user.Sign(ctorArgs...)
 	Expect(err).NotTo(HaveOccurred())
 
 	ctorArgs = append(ctorArgs, pubKey, base58.Encode(sMsg))
-
-	lh := nwo.GetLedgerHeight(network, peer, channel)
-	sess, err := network.PeerUserSession(peer, "User1", commands.ChaincodeInvoke{
-		ChannelID: channel,
-		Orderer:   network.OrdererAddress(orderer, nwo.ListenPort),
-		Name:      ccName,
-		Ctor:      cmn.CtorFromSlice(ctorArgs),
-		PeerAddresses: []string{
-			network.PeerAddress(network.Peer("Org1", "peer0"), nwo.ListenPort),
-			network.PeerAddress(network.Peer("Org2", "peer0"), nwo.ListenPort),
-		},
-		WaitForEvent: true,
-	})
-	Expect(err).NotTo(HaveOccurred())
-	Eventually(sess, network.EventuallyTimeout).Should(gexec.Exit(0))
-	Expect(sess.Err).To(gbytes.Say("Chaincode invoke successful. result: status:200"))
-
-	l := sess.Err.Contents()
-	txId := scanTxIDInLog(l)
-	Expect(txId).NotTo(BeEmpty())
-
-	By("getting the signer for user1 on peer " + peer.ID())
-	signer := network.PeerUserSigner(peer, "User1")
-
-	By("creating the deliver client to peer " + peer.ID())
-	pcc := network.PeerClientConn(peer)
-	defer pcc.Close()
-	ctx, cancel := context.WithTimeout(context.Background(), network.EventuallyTimeout)
-	defer cancel()
-	dc, err := pb.NewDeliverClient(pcc).Deliver(ctx)
-	Expect(err).NotTo(HaveOccurred())
-	defer dc.CloseSend()
-
-	By("starting filtered delivery on peer " + peer.ID())
-	deliverEnvelope, err := protoutil.CreateSignedEnvelope(
-		cb.HeaderType_DELIVER_SEEK_INFO,
-		channel,
-		signer,
-		&ab.SeekInfo{
-			Behavior: ab.SeekInfo_BLOCK_UNTIL_READY,
-			Start: &ab.SeekPosition{
-				Type: &ab.SeekPosition_Specified{
-					Specified: &ab.SeekSpecified{Number: uint64(lh)},
-				},
-			},
-			Stop: &ab.SeekPosition{
-				Type: &ab.SeekPosition_Specified{
-					Specified: &ab.SeekSpecified{Number: math.MaxUint64},
-				},
-			},
-		},
-		0,
-		0,
-	)
-	Expect(err).NotTo(HaveOccurred())
-	err = dc.Send(deliverEnvelope)
-	Expect(err).NotTo(HaveOccurred())
-
-	By("waiting for deliver event on peer " + peer.ID())
-	for {
-		resp, err := dc.Recv()
-		Expect(err).NotTo(HaveOccurred())
-
-		b, ok := resp.Type.(*pb.DeliverResponse_Block)
-		Expect(ok).To(BeTrue())
-
-		txFilter := b.Block.GetMetadata().GetMetadata()[common.BlockMetadataIndex_TRANSACTIONS_FILTER]
-		for txIndex, ebytes := range b.Block.GetData().GetData() {
-			var env *common.Envelope
-
-			if ebytes == nil {
-				continue
-			}
-
-			isValid := true
-			if len(txFilter) != 0 &&
-				pb.TxValidationCode(txFilter[txIndex]) != pb.TxValidationCode_VALID {
-				isValid = false
-			}
-
-			env, err = protoutil.GetEnvelopeFromBlock(ebytes)
-			if err != nil {
-				continue
-			}
-
-			// get the payload from the envelope
-			payload, err := protoutil.UnmarshalPayload(env.Payload)
-			Expect(err).NotTo(HaveOccurred())
-
-			if payload.Header == nil {
-				continue
-			}
-
-			chdr, err := protoutil.UnmarshalChannelHeader(payload.Header.ChannelHeader)
-			Expect(err).NotTo(HaveOccurred())
-
-			if common.HeaderType(chdr.GetType()) != common.HeaderType_ENDORSER_TRANSACTION {
-				continue
-			}
-
-			tx, err := protoutil.UnmarshalTransaction(payload.GetData())
-			Expect(err).NotTo(HaveOccurred())
-
-			for _, action := range tx.GetActions() {
-				chaincodeActionPayload, err := protoutil.UnmarshalChaincodeActionPayload(action.Payload)
-				Expect(err).NotTo(HaveOccurred())
-
-				if chaincodeActionPayload.Action == nil {
-					continue
-				}
-
-				propRespPayload, err := protoutil.UnmarshalProposalResponsePayload(chaincodeActionPayload.Action.ProposalResponsePayload)
-				Expect(err).NotTo(HaveOccurred())
-
-				caPayload, err := protoutil.UnmarshalChaincodeAction(propRespPayload.Extension)
-				Expect(err).NotTo(HaveOccurred())
-
-				ccEvent, err := protoutil.UnmarshalChaincodeEvents(caPayload.Events)
-				Expect(err).NotTo(HaveOccurred())
-
-				if ccEvent.GetEventName() == "batchExecute" {
-					batchResponse := &pbfound.BatchResponse{}
-					err = proto.Unmarshal(caPayload.Response.Payload, batchResponse)
-					Expect(err).NotTo(HaveOccurred())
-
-					for _, r := range batchResponse.TxResponses {
-						if hex.EncodeToString(r.GetId()) == txId {
-							Expect(isValid).To(BeTrue())
-
-							Expect(r.Error).NotTo(BeNil())
-
-							Expect(r.Error.Error).To(Equal(errMsg))
-							return
-						}
-					}
-				}
-			}
-		}
-	}
+	return invokeTx(network, peer, orderer, "User1", channel, ccName, errMsg, args...)
 }
 
 func scanTxIDInLog(data []byte) string {
