df3b77e76352e05f32c2fe21820bd086cd20e8c4
Gennadii Muzykantov <gennadii@muzykantov.me>
2024-07-12 12:36:25 +0300

    Remove unused godoc.
    Signed-off-by: Gennadii Muzykantov <gennadii@muzykantov.me>

--- core/cc_invoke_router.go
+++ core/cc_invoke_router.go
@@ -8,28 +8,8 @@ import (
 	"go.opentelemetry.io/otel/codes"
 )
 
-// InvokeContractMethod calls a Chaincode contract method, processes the arguments, and returns the result as bytes.
-//
-// Parameters:
-//   - traceCtx: The telemetry trace context for tracing the method invocation.
-//   - stub: The ChaincodeStubInterface instance used for invoking the method.
-//   - method: The routing.Method instance representing the method to be invoked.
-//   - sender: The sender's address, if the method requires authentication.
-//   - args: A slice of strings representing the arguments to be passed to the method.
-//   - cfgBytes: A byte slice containing the configuration data for the contract.
-//
-// Returns:
-//   - A byte slice containing the serialized return value of the method, or an error if an issue occurs.
-//
-// The function performs the following steps:
-//  1. Initializes a new span for tracing.
-//  2. Adds the sender's address to the arguments if provided.
-//  3. Sets trace attributes for the arguments.
-//  4. Checks the number of arguments, ensuring it matches the expected count.
-//  5. Applies the configuration data to the contract.
-//  6. Calls the contract method via the router.
-//  7. Processes the return error if the method returns an error.
-//  8. Sets the trace status to Ok if no error occurs and returns the result.
+// InvokeContractMethod calls a Chaincode contract method, processes the arguments, and
+// returns the result as bytes.
 func (cc *Chaincode) InvokeContractMethod(
 	traceCtx telemetry.TraceContext,
 	stub shim.ChaincodeStubInterface,
