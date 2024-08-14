cf74353747a6451029da58a94559d819ce57d490
Dmitry Atapin <dmitry.atapin.83@gmail.com>
2024-08-14 15:21:00 +0300

    Basic key types test migrated to testsuite
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>

--- test/integration/basic/basic_key_types_test.go
+++ test/integration/basic/basic_key_types_test.go
@@ -2,102 +2,48 @@ package basic
 
 import (
 	"encoding/json"
-	"github.com/hyperledger/fabric/integration"
-	"os"
-	"syscall"
-	"time"
-
 	pbfound "github.com/anoideaopen/foundation/proto"
 	"github.com/anoideaopen/foundation/test/integration/cmn"
 	"github.com/anoideaopen/foundation/test/integration/cmn/client"
-	"github.com/anoideaopen/foundation/test/integration/cmn/runner"
-	docker "github.com/fsouza/go-dockerclient"
-	runnerFbk "github.com/hyperledger/fabric/integration/nwo/runner"
+	"github.com/hyperledger/fabric/integration"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
-	"github.com/tedsuo/ifrit"
 )
 
 var _ = Describe("Basic foundation tests with different key types", func() {
 	var (
-		testDir          string
-		cli              *docker.Client
-		networkProcess   ifrit.Process
-		ordererProcesses []ifrit.Process
-		peerProcesses    ifrit.Process
-		ts               client.TestSuite
+		ts       client.TestSuite
+		channels = []string{cmn.ChannelAcl, cmn.ChannelCC, cmn.ChannelFiat, cmn.ChannelIndustrial}
 	)
 
 	BeforeEach(func() {
-		networkProcess = nil
-		ordererProcesses = nil
-		peerProcesses = nil
-		var err error
-		testDir, err = os.MkdirTemp("", "foundation")
-		Expect(err).NotTo(HaveOccurred())
-
-		cli, err = docker.NewClientFromEnv()
-		Expect(err).NotTo(HaveOccurred())
+		ts = client.NewTestSuite(components, channels)
 	})
 
 	AfterEach(func() {
-		if networkProcess != nil {
-			networkProcess.Signal(syscall.SIGTERM)
-			Eventually(networkProcess.Wait(), ts.Network().EventuallyTimeout).Should(Receive())
-		}
-		if peerProcesses != nil {
-			peerProcesses.Signal(syscall.SIGTERM)
-			Eventually(peerProcesses.Wait(), ts.Network().EventuallyTimeout).Should(Receive())
-		}
-		if ts.Network() != nil {
-			ts.Network().Cleanup()
-		}
-		for _, ordererInstance := range ordererProcesses {
-			ordererInstance.Signal(syscall.SIGTERM)
-			Eventually(ordererInstance.Wait(), ts.Network().EventuallyTimeout).Should(Receive())
-		}
-		err := os.RemoveAll(testDir)
-		Expect(err).NotTo(HaveOccurred())
+		ts.ShutdownNetwork()
 	})
 
 	Describe("foundation test", func() {
-		var (
-			channels     = []string{cmn.ChannelAcl, cmn.ChannelCC, cmn.ChannelFiat, cmn.ChannelIndustrial}
-			redisProcess ifrit.Process
-			redisDB      *runner.RedisDB
-			robotProc    ifrit.Process
-		)
 		BeforeEach(func() {
 			By("start redis")
-			redisDB = &runner.RedisDB{}
-			redisProcess = ifrit.Invoke(redisDB)
-			Eventually(redisProcess.Ready(), runnerFbk.DefaultStartTimeout).Should(BeClosed())
-			Consistently(redisProcess.Wait()).ShouldNot(Receive())
+			ts.StartRedis()
 		})
 		BeforeEach(func() {
-			ts = client.NewTestSuite("", "", "", "", redisDB.Address(), channels, testDir, cli, integration.DevModePort, components, ordererProcesses, peerProcesses)
+			ts.InitNetwork(integration.DevModePort)
 			ts.DeployChannels()
 		})
 		BeforeEach(func() {
 			By("start robot")
-			robotRunner := ts.NetworkFound().RobotRunner()
-			robotProc = ifrit.Invoke(robotRunner)
-			Eventually(robotProc.Ready(), ts.Network().EventuallyTimeout).Should(BeClosed())
+			ts.StartRobot()
 		})
 		AfterEach(func() {
 			By("stop robot")
-			if robotProc != nil {
-				robotProc.Signal(syscall.SIGTERM)
-				Eventually(robotProc.Wait(), ts.Network().EventuallyTimeout).Should(Receive())
-			}
-		})
-		AfterEach(func() {
-			By("stop redis " + redisDB.Address())
-			if redisProcess != nil {
-				redisProcess.Signal(syscall.SIGTERM)
-				Eventually(redisProcess.Wait(), time.Minute).Should(Receive())
-			}
+			ts.StopRobot()
+
+			By("stop redis")
+			ts.StopRedis()
 		})
 
 		It("transfer", func() {
@@ -112,7 +58,7 @@ var _ = Describe("Basic foundation tests with different key types", func() {
 			ts.AddUser(user2)
 
 			By("add admin to acl")
-			ts.AddUser(ts.Admin())
+			ts.AddAdminToACL()
 
 			By("emit tokens")
 			amount := "1"
