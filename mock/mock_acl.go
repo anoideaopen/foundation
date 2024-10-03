46eaf2b50087dfe8c08d572d01fce705a3b46001
Dmitry Atapin <dmitry.atapin.83@gmail.com>
2024-10-03 15:50:22 +0300

    Global testsuite & nominee methods added
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>

--- mock/mock_acl.go
+++ mock/mock_acl.go
@@ -16,9 +16,13 @@ import (
 	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/hyperledger/fabric-protos-go/peer"
 	"golang.org/x/crypto/sha3"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
-const rightKey = "acl_access_matrix"
+const (
+	keyRight        = "acl_access_matrix"
+	keyAddressRight = "acl_access_matrix_principal_addresses"
+)
 
 // mockACL emulates alc chaincode, rights are stored in state
 type mockACL struct{}
@@ -30,7 +34,7 @@ func (ma *mockACL) Init(_ shim.ChaincodeStubInterface) peer.Response { // stub
 func (ma *mockACL) Invoke(stub shim.ChaincodeStubInterface) peer.Response { //nolint:funlen,gocognit
 	fn, args := stub.GetFunctionAndParameters()
 	switch fn {
-	case "checkAddress":
+	case acl.FnCheckAddress:
 		addr, err := types.AddrFromBase58Check(args[0])
 		if err != nil {
 			return shim.Error(err.Error())
@@ -41,7 +45,7 @@ func (ma *mockACL) Invoke(stub shim.ChaincodeStubInterface) peer.Response { //no
 			return shim.Error(err.Error())
 		}
 		return shim.Success(data)
-	case "checkKeys":
+	case acl.FnCheckKeys:
 		keys := strings.Split(args[0], "/")
 		binPubKeys := make([][]byte, len(keys))
 		for i, k := range keys {
@@ -73,7 +77,7 @@ func (ma *mockACL) Invoke(stub shim.ChaincodeStubInterface) peer.Response { //no
 			return shim.Error(err.Error())
 		}
 		return shim.Success(data)
-	case "getAccountInfo":
+	case acl.FnGetAccountInfo:
 		data, err := json.Marshal(&pb.AccountInfo{
 			KycHash:     "123",
 			GrayListed:  false,
@@ -83,9 +87,9 @@ func (ma *mockACL) Invoke(stub shim.ChaincodeStubInterface) peer.Response { //no
 			return shim.Error(err.Error())
 		}
 		return shim.Success(data)
-	case acl.GetAccOpRightFn:
-		if len(args) != acl.GetAccOpRightArgCount {
-			return shim.Error(fmt.Sprintf(acl.WrongArgsCount, len(args), acl.GetAccOpRightArgCount))
+	case acl.FnGetAccOpRight:
+		if len(args) != acl.ArgsQtyGetAccOpRight {
+			return shim.Error(fmt.Sprintf(acl.ErrWrongArgsCount, len(args), acl.ArgsQtyGetAccOpRight))
 		}
 
 		ch, cc, role, operation, addr := args[0], args[1], args[2], args[3], args[4]
@@ -99,9 +103,9 @@ func (ma *mockACL) Invoke(stub shim.ChaincodeStubInterface) peer.Response { //no
 			return shim.Error(err.Error())
 		}
 		return shim.Success(rawResultData)
-	case acl.AddRightsFn:
-		if len(args) != acl.AddRightsArgsCount {
-			return shim.Error(fmt.Sprintf(acl.WrongArgsCount, len(args), acl.AddRightsArgsCount))
+	case acl.FnAddRights:
+		if len(args) != acl.ArgsQtyAddRights {
+			return shim.Error(fmt.Sprintf(acl.ErrWrongArgsCount, len(args), acl.ArgsQtyAddRights))
 		}
 
 		ch, cc, role, operation, addr := args[0], args[1], args[2], args[3], args[4]
@@ -111,9 +115,9 @@ func (ma *mockACL) Invoke(stub shim.ChaincodeStubInterface) peer.Response { //no
 		}
 
 		return shim.Success(nil)
-	case acl.RemoveRightsFn:
-		if len(args) != acl.RemoveRightsArgsCount {
-			return shim.Error(fmt.Sprintf(acl.WrongArgsCount, len(args), acl.RemoveRightsArgsCount))
+	case acl.FnRemoveRights:
+		if len(args) != acl.ArgsQtyRemoveRights {
+			return shim.Error(fmt.Sprintf(acl.ErrWrongArgsCount, len(args), acl.ArgsQtyRemoveRights))
 		}
 
 		ch, cc, role, operation, addr := args[0], args[1], args[2], args[3], args[4]
@@ -123,7 +127,7 @@ func (ma *mockACL) Invoke(stub shim.ChaincodeStubInterface) peer.Response { //no
 		}
 
 		return shim.Success(nil)
-	case "getAccountsInfo":
+	case acl.FnGetAccountsInfo:
 		responses := make([]peer.Response, 0)
 		for _, a := range args {
 			var argsTmp []string
@@ -148,13 +152,69 @@ func (ma *mockACL) Invoke(stub shim.ChaincodeStubInterface) peer.Response { //no
 			return shim.Error(fmt.Sprintf("failed get accounts info: marshal GetAccountsInfoResponse: %s", err))
 		}
 		return shim.Success(b)
+	case acl.FnAddAddressRightForNominee:
+		if len(args) != acl.ArgsQtyAddAddressRightForNominee {
+			return shim.Error(fmt.Sprintf(acl.ErrWrongArgsCount, len(args), acl.ArgsQtyAddAddressRightForNominee))
+		}
+
+		channelName, chaincodeName, nomineeAddress, principalAddress := args[0], args[1], args[2], args[3]
+		err := ma.addAddressRightForNominee(stub, channelName, chaincodeName, nomineeAddress, principalAddress)
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+
+		return shim.Success(nil)
+	case acl.FnRemoveAddressRightFromNominee:
+		if len(args) != acl.ArgsQtyRemoveAddressRightFromNominee {
+			return shim.Error(fmt.Sprintf(acl.ErrWrongArgsCount, len(args), acl.ArgsQtyRemoveAddressRightFromNominee))
+		}
+
+		channelName, chaincodeName, nomineeAddress, principalAddress := args[0], args[1], args[2], args[3]
+		err := ma.removeAddressRightFromNominee(stub, channelName, chaincodeName, nomineeAddress, principalAddress)
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+
+		return shim.Success(nil)
+	case acl.FnGetAddressRightForNominee:
+		if len(args) != acl.ArgsQtyGetAddressRightForNominee {
+			return shim.Error(fmt.Sprintf(acl.ErrWrongArgsCount, len(args), acl.ArgsQtyGetAddressRightForNominee))
+		}
+
+		channelName, chaincodeName, nomineeAddress, principalAddress := args[0], args[1], args[2], args[3]
+		haveRight, err := ma.getAddressRightForNominee(stub, channelName, chaincodeName, nomineeAddress, principalAddress)
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		rawResultData, err := protojson.MarshalOptions{EmitUnpopulated: true}.Marshal(&pb.HaveRight{HaveRight: haveRight})
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		return shim.Success(rawResultData)
+	case acl.FnGetAddressesListForNominee:
+		if len(args) != acl.ArgsQtyGetAddressesListForNominee {
+			return shim.Error(fmt.Sprintf(acl.ErrWrongArgsCount, len(args), acl.ArgsQtyGetAddressesListForNominee))
+		}
+
+		channelName, chaincodeName, nomineeAddress := args[0], args[1], args[2]
+		addresses, err := ma.getAddressesListForNominee(stub, channelName, chaincodeName, nomineeAddress)
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+
+		rawAddresses, err := protojson.MarshalOptions{EmitUnpopulated: true}.Marshal(addresses)
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+
+		return shim.Success(rawAddresses)
 	default:
 		panic("should not be here")
 	}
 }
 
 func (ma *mockACL) addRight(stub shim.ChaincodeStubInterface, channel, cc, role, addr, operation string) error {
-	key, err := stub.CreateCompositeKey(rightKey, []string{channel, cc, role, operation})
+	key, err := stub.CreateCompositeKey(keyRight, []string{channel, cc, role, operation})
 	if err != nil {
 		return err
 	}
@@ -198,7 +258,7 @@ func (ma *mockACL) addRight(stub shim.ChaincodeStubInterface, channel, cc, role,
 }
 
 func (ma *mockACL) removeRight(stub shim.ChaincodeStubInterface, channel, cc, role, addr, operation string) error {
-	key, err := stub.CreateCompositeKey(rightKey, []string{channel, cc, role, operation})
+	key, err := stub.CreateCompositeKey(keyRight, []string{channel, cc, role, operation})
 	if err != nil {
 		return err
 	}
@@ -240,7 +300,7 @@ func (ma *mockACL) removeRight(stub shim.ChaincodeStubInterface, channel, cc, ro
 }
 
 func (ma *mockACL) getRight(stub shim.ChaincodeStubInterface, channel, cc, role, addr, operation string) (bool, error) {
-	key, err := stub.CreateCompositeKey(rightKey, []string{channel, cc, role, operation})
+	key, err := stub.CreateCompositeKey(keyRight, []string{channel, cc, role, operation})
 	if err != nil {
 		return false, err
 	}
@@ -275,4 +335,173 @@ func (ma *mockACL) getRight(stub shim.ChaincodeStubInterface, channel, cc, role,
 	}
 
 	return false, nil
+}
+
+func (ma *mockACL) addAddressRightForNominee(
+	stub shim.ChaincodeStubInterface,
+	channelName string,
+	chaincodeName string,
+	nomineeAddress string,
+	principalAddress string,
+) error {
+	key, err := stub.CreateCompositeKey(keyAddressRight, []string{channelName, chaincodeName, nomineeAddress})
+	if err != nil {
+		return err
+	}
+
+	rawAddresses, err := stub.GetState(key)
+	if err != nil {
+		return err
+	}
+	addresses := &pb.Accounts{Addresses: []*pb.Address{}}
+	if len(rawAddresses) != 0 {
+		err = protojson.Unmarshal(rawAddresses, addresses)
+		if err != nil {
+			return err
+		}
+	}
+
+	value, ver, err := base58.CheckDecode(principalAddress)
+	if err != nil {
+		return err
+	}
+	address := pb.Address{Address: append([]byte{ver}, value...)[:32]}
+
+	for _, existedAddr := range addresses.GetAddresses() {
+		if address.String() == existedAddr.String() {
+			return nil
+		}
+	}
+
+	addresses.Addresses = append(addresses.Addresses, &address)
+	rawAddresses, err = protojson.MarshalOptions{EmitUnpopulated: true}.Marshal(addresses)
+	if err != nil {
+		return err
+	}
+
+	err = stub.PutState(key, rawAddresses)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ma *mockACL) removeAddressRightFromNominee(
+	stub shim.ChaincodeStubInterface,
+	channelName string,
+	chaincodeName string,
+	nomineeAddress string,
+	principalAddress string,
+) error {
+	key, err := stub.CreateCompositeKey(keyAddressRight, []string{channelName, chaincodeName, nomineeAddress})
+	if err != nil {
+		return err
+	}
+
+	rawAddresses, err := stub.GetState(key)
+	if err != nil {
+		return err
+	}
+	addresses := &pb.Accounts{Addresses: []*pb.Address{}}
+	if len(rawAddresses) != 0 {
+		err := protojson.Unmarshal(rawAddresses, addresses)
+		if err != nil {
+			return err
+		}
+	}
+
+	value, ver, err := base58.CheckDecode(principalAddress)
+	if err != nil {
+		return err
+	}
+	address := pb.Address{Address: append([]byte{ver}, value...)[:32]}
+
+	for i, existedAddr := range addresses.GetAddresses() {
+		if existedAddr.String() == address.String() {
+			addresses.Addresses = append(addresses.Addresses[:i], addresses.GetAddresses()[i+1:]...)
+			rawAddresses, err = protojson.MarshalOptions{EmitUnpopulated: true}.Marshal(addresses)
+			if err != nil {
+				return err
+			}
+			err = stub.PutState(key, rawAddresses)
+			if err != nil {
+				return err
+			}
+			break
+		}
+	}
+
+	return nil
+}
+
+func (ma *mockACL) getAddressRightForNominee(
+	stub shim.ChaincodeStubInterface,
+	channelName string,
+	chaincodeName string,
+	nomineeAddress string,
+	principalAddress string,
+) (bool, error) {
+	key, err := stub.CreateCompositeKey(keyAddressRight, []string{channelName, chaincodeName, nomineeAddress})
+	if err != nil {
+		return false, err
+	}
+
+	rawAddresses, err := stub.GetState(key)
+	if err != nil {
+		return false, err
+	}
+
+	if len(rawAddresses) == 0 {
+		return false, nil
+	}
+
+	addrs := &pb.Accounts{Addresses: []*pb.Address{}}
+	if len(rawAddresses) != 0 {
+		err = protojson.Unmarshal(rawAddresses, addrs)
+		if err != nil {
+			return false, err
+		}
+	}
+
+	value, ver, err := base58.CheckDecode(principalAddress)
+	if err != nil {
+		return false, err
+	}
+	address := pb.Address{Address: append([]byte{ver}, value...)[:32]}
+
+	for _, existedAddr := range addrs.GetAddresses() {
+		if existedAddr.String() == address.String() {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func (ma *mockACL) getAddressesListForNominee(
+	stub shim.ChaincodeStubInterface,
+	channelName string,
+	chaincodeName string,
+	nomineeAddress string,
+) (*pb.Accounts, error) {
+	key, err := stub.CreateCompositeKey(keyAddressRight, []string{channelName, chaincodeName, nomineeAddress})
+	if err != nil {
+		return nil, err
+	}
+
+	rawAddresses, err := stub.GetState(key)
+	if err != nil {
+		return nil, err
+	}
+
+	addrs := &pb.Accounts{Addresses: []*pb.Address{}}
+	if len(rawAddresses) != 0 {
+		err = protojson.Unmarshal(rawAddresses, addrs)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return addrs, nil
 }
