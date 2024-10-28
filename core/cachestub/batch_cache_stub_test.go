f097bb4d1bce3ff79e1a8a6ef20cc9821fb7a63f
Dmitry Atapin <dmitry.atapin.83@gmail.com>
2024-10-28 23:28:04 +0300

    batch cache stub test migrated to chaincode stub
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>

--- core/cachestub/batch_cache_stub_test.go
+++ core/cachestub/batch_cache_stub_test.go
@@ -1,9 +1,9 @@
 package cachestub
 
 import (
-	"github.com/anoideaopen/foundation/core/cachestub/mock"
 	"testing"
 
+	"github.com/anoideaopen/foundation/mocks"
 	"github.com/stretchr/testify/require"
 )
 
@@ -26,52 +26,72 @@ const (
 )
 
 func TestBatchStub(t *testing.T) {
-	stateStub := mock.NewStateStub()
-
-	// preparing cacheStub values
-	_ = stateStub.PutState(valKey1, []byte(valKey1Value1))
-	_ = stateStub.PutState(valKey2, []byte(valKey2Value1))
-	_ = stateStub.PutState(valKey3, []byte(valKey3Value1))
+	stateStub := &mocks.ChaincodeStub{}
 
 	// creating batch cache stub
 	batchStub := NewBatchCacheStub(stateStub)
 
-	// changing key1 value
-	_ = batchStub.PutState(valKey1, []byte(valKey1Value2))
-	// deleting key2 value
-	_ = batchStub.DelState(valKey2)
-	// committing changes to mockStub
-	_ = batchStub.Commit()
+	// 1st transaction changes key 1 value and deletes from key 2
+	t.Run("first batch transaction", func(t *testing.T) {
+		// changing key1 value
+		_ = batchStub.PutState(valKey1, []byte(valKey1Value1))
+		// deleting key2 value
+		_ = batchStub.DelState(valKey2)
+		// committing changes to mockStub
+		_ = batchStub.Commit()
 
-	// checking key2 value is deleted
-	val1, _ := batchStub.GetState(valKey2)
-	require.Equal(t, "", string(val1))
+		// checking key1 value added
+		val, _ := batchStub.GetState(valKey1)
+		require.Equal(t, valKey1Value1, string(val))
 
-	// checking key1 value changed
-	val2, _ := batchStub.GetState(valKey1)
-	require.Equal(t, valKey1Value2, string(val2))
+		// checking key2 value is deleted
+		val, _ = batchStub.GetState(valKey2)
+		require.Equal(t, "", string(val))
 
-	// setting key2 value 2
-	_ = batchStub.PutState(valKey2, []byte(valKey2Value2))
-	// deleting key3 value
-	_ = batchStub.DelState(valKey3)
-	// committing changes to mock stub
-	_ = batchStub.Commit()
+		// checking mock stub calls
+		require.Equal(t, 0, stateStub.GetStateCallCount())
+		require.Equal(t, 1, stateStub.PutStateCallCount())
+		require.Equal(t, 1, stateStub.DelStateCallCount())
+	})
 
-	// checking mock stub state length
-	require.Equal(t, 2, len(stateStub.State))
-	// checking mock stub key1 value
-	require.Equal(t, valKey1Value2, string(stateStub.State[valKey1]))
-	// checking mock stub key2 value
-	require.Equal(t, valKey2Value2, string(stateStub.State[valKey2]))
+	// 2nd batch transaction changes key 2  value and deletes key 3
+	t.Run("second batch transaction", func(t *testing.T) {
+		// adding key2 value
+		_ = batchStub.PutState(valKey2, []byte(valKey2Value1))
+		// deleting key3 value
+		_ = batchStub.DelState(valKey3)
+		// committing changes to mock stub
+		_ = batchStub.Commit()
 
-	// adding key4 value
-	_ = batchStub.PutState(valKey4, []byte(valKey4Value1))
-	// deleting key4 value
-	_ = batchStub.DelState(valKey4)
-	// committing changes to mock stub
-	_ = batchStub.Commit()
+		// checking key2 value added
+		val, _ := batchStub.GetState(valKey2)
+		require.Equal(t, valKey2Value1, string(val))
 
-	_, ok := stateStub.State[valKey4]
-	require.Equal(t, false, ok)
+		// checking key3 value is deleted
+		val, _ = batchStub.GetState(valKey3)
+		require.Equal(t, "", string(val))
+
+		// checking mock stub calls
+		require.Equal(t, 0, stateStub.GetStateCallCount())
+		require.Equal(t, 3, stateStub.PutStateCallCount())
+		require.Equal(t, 2, stateStub.DelStateCallCount())
+	})
+
+	// 3rd batch transaction adds and deletes key 4 value
+	t.Run("third batch transaction", func(t *testing.T) {
+		// adding key4 value
+		_ = batchStub.PutState(valKey4, []byte(valKey4Value1))
+		// deleting key4 value
+		_ = batchStub.DelState(valKey4)
+		// committing changes to mock stub
+		_ = batchStub.Commit()
+
+		val, _ := batchStub.GetState(valKey4)
+		require.Equal(t, "", string(val))
+
+		// checking mock stub calls
+		require.Equal(t, 0, stateStub.GetStateCallCount())
+		require.Equal(t, 5, stateStub.PutStateCallCount())
+		require.Equal(t, 4, stateStub.DelStateCallCount())
+	})
 }
