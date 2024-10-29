39bee1150bf143831d27182b9b39212f1a97228f
Dmitry Atapin <dmitry.atapin.83@gmail.com>
2024-10-29 18:12:27 +0300

    batch cache stub modified
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>

--- core/cachestub/batch_cache_stub_test.go
+++ core/cachestub/batch_cache_stub_test.go
@@ -1,6 +1,7 @@
 package cachestub
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/anoideaopen/foundation/mocks"
@@ -15,9 +16,13 @@ const (
 	valKey4 = "KEY4"
 
 	valKey1Value1 = "key1_value1"
+	valKey1Value2 = "key1_value2"
 	valKey2Value1 = "key2_value1"
+	valKey3Value1 = "key3_value1"
 	valKey4Value1 = "key4_value1"
 )
+
+var testError = fmt.Errorf("test error")
 
 //go:generate counterfeiter -generate
 
@@ -27,72 +32,211 @@ type chaincodeStub interface {
 }
 
 func TestBatchStub(t *testing.T) {
-	stateStub := &mocks.ChaincodeStub{}
 
-	// creating batch cache stub
-	batchStub := NewBatchCacheStub(stateStub)
+	t.Run("GetState test", func(t *testing.T) {
+		stateStub := &mocks.ChaincodeStub{}
+		stateStub.GetStateReturns([]byte(valKey1Value1), nil)
+		// creating batch cache stub
+		batchStub := NewBatchCacheStub(stateStub)
 
-	// 1st transaction changes key 1 value and deletes from key 2
-	t.Run("first batch transaction", func(t *testing.T) {
-		// changing key1 value
-		_ = batchStub.PutState(valKey1, []byte(valKey1Value1))
-		// deleting key2 value
-		_ = batchStub.DelState(valKey2)
-		// committing changes to mockStub
-		_ = batchStub.Commit()
-
-		// checking key1 value added
-		val, _ := batchStub.GetState(valKey1)
-		require.Equal(t, valKey1Value1, string(val))
-
-		// checking key2 value is deleted
-		val, _ = batchStub.GetState(valKey2)
-		require.Equal(t, "", string(val))
+		// requesting data from state
+		result, err := batchStub.GetState(valKey1)
+		require.NoError(t, err)
+		require.Equal(t, []byte(valKey1Value1), result)
 
 		// checking mock stub calls
-		require.Equal(t, 0, stateStub.GetStateCallCount())
-		require.Equal(t, 1, stateStub.PutStateCallCount())
-		require.Equal(t, 1, stateStub.DelStateCallCount())
+		require.Equal(t, 1, stateStub.GetStateCallCount())
 	})
 
-	// 2nd batch transaction changes key 2  value and deletes key 3
-	t.Run("second batch transaction", func(t *testing.T) {
-		// adding key2 value
-		_ = batchStub.PutState(valKey2, []byte(valKey2Value1))
-		// deleting key3 value
-		_ = batchStub.DelState(valKey3)
-		// committing changes to mock stub
-		_ = batchStub.Commit()
+	t.Run("[negative] GetState error test", func(t *testing.T) {
+		stateStub := &mocks.ChaincodeStub{}
+		stateStub.GetStateReturns(nil, testError)
+		// creating batch cache stub
+		batchStub := NewBatchCacheStub(stateStub)
 
-		// checking key2 value added
-		val, _ := batchStub.GetState(valKey2)
-		require.Equal(t, valKey2Value1, string(val))
+		// requesting data from state
+		_, err := batchStub.GetState(valKey1)
+		require.Errorf(t, err, testError.Error())
 
-		// checking key3 value is deleted
-		val, _ = batchStub.GetState(valKey3)
-		require.Equal(t, "", string(val))
+		// checking mock stub calls
+		require.Equal(t, 1, stateStub.GetStateCallCount())
+	})
+
+	t.Run("PutState test", func(t *testing.T) {
+		stateStub := &mocks.ChaincodeStub{}
+		// creating batch cache stub
+		batchStub := NewBatchCacheStub(stateStub)
+
+		// sending data to state
+		err := batchStub.PutState(valKey1, []byte(valKey1Value1))
+		require.NoError(t, err)
+
+		err = batchStub.PutState(valKey1, []byte(valKey1Value2))
+		require.NoError(t, err)
+
+		err = batchStub.PutState(valKey2, []byte(valKey2Value1))
+		require.NoError(t, err)
+
+		// checking added data
+		result, err := batchStub.GetState(valKey1)
+		require.NoError(t, err)
+		require.Equal(t, []byte(valKey1Value2), result)
+
+		result, err = batchStub.GetState(valKey2)
+		require.NoError(t, err)
+		require.Equal(t, []byte(valKey2Value1), result)
+
+		// checking mock stub calls
+		require.Equal(t, 0, stateStub.PutStateCallCount())
+
+		// committing batch stub data
+		err = batchStub.Commit()
+		require.NoError(t, err)
 
 		// checking mock stub calls
 		require.Equal(t, 0, stateStub.GetStateCallCount())
-		require.Equal(t, 3, stateStub.PutStateCallCount())
+		require.Equal(t, 2, stateStub.PutStateCallCount())
+	})
+
+	t.Run("[negative] PutState error test", func(t *testing.T) {
+		stateStub := &mocks.ChaincodeStub{}
+		stateStub.PutStateReturns(testError)
+		// creating batch cache stub
+		batchStub := NewBatchCacheStub(stateStub)
+
+		err := batchStub.PutState(valKey1, []byte(valKey1Value1))
+		require.NoError(t, err)
+
+		err = batchStub.Commit()
+		require.Errorf(t, err, testError.Error())
+
+		// checking mock stub calls
+		require.Equal(t, 1, stateStub.PutStateCallCount())
+	})
+
+	t.Run("DelState test", func(t *testing.T) {
+		stateStub := &mocks.ChaincodeStub{}
+		// creating batch cache stub
+		batchStub := NewBatchCacheStub(stateStub)
+
+		// deleting data from state
+		err := batchStub.DelState(valKey1)
+		require.NoError(t, err)
+
+		err = batchStub.DelState(valKey1)
+		require.NoError(t, err)
+
+		err = batchStub.DelState(valKey2)
+		require.NoError(t, err)
+
+		// checking data deleted
+		result, err := batchStub.GetState(valKey1)
+		require.NoError(t, err)
+		require.Equal(t, []byte(nil), result)
+
+		result, err = batchStub.GetState(valKey2)
+		require.NoError(t, err)
+		require.Equal(t, []byte(nil), result)
+
+		// checking mock stub calls
+		require.Equal(t, 0, stateStub.DelStateCallCount())
+
+		// committing batch stub data
+		err = batchStub.Commit()
+		require.NoError(t, err)
+
+		// checking mock stub calls
+		require.Equal(t, 0, stateStub.GetStateCallCount())
 		require.Equal(t, 2, stateStub.DelStateCallCount())
 	})
 
-	// 3rd batch transaction adds and deletes key 4 value
-	t.Run("third batch transaction", func(t *testing.T) {
-		// adding key4 value
-		_ = batchStub.PutState(valKey4, []byte(valKey4Value1))
-		// deleting key4 value
-		_ = batchStub.DelState(valKey4)
-		// committing changes to mock stub
-		_ = batchStub.Commit()
+	t.Run("[negative] DelState error test", func(t *testing.T) {
+		stateStub := &mocks.ChaincodeStub{}
+		stateStub.DelStateReturns(testError)
+		// creating batch cache stub
+		batchStub := NewBatchCacheStub(stateStub)
 
-		val, _ := batchStub.GetState(valKey4)
-		require.Equal(t, "", string(val))
+		// deleting data from state
+		err := batchStub.DelState(valKey1)
+		require.NoError(t, err)
+
+		err = batchStub.Commit()
+		require.Errorf(t, err, testError.Error())
 
 		// checking mock stub calls
-		require.Equal(t, 0, stateStub.GetStateCallCount())
-		require.Equal(t, 5, stateStub.PutStateCallCount())
-		require.Equal(t, 4, stateStub.DelStateCallCount())
+		require.Equal(t, 1, stateStub.DelStateCallCount())
+	})
+
+	t.Run("Mixed test", func(t *testing.T) {
+		stateStub := &mocks.ChaincodeStub{}
+
+		// preparing state stub
+		stateStub.GetStateReturnsOnCall(0, []byte(valKey1Value1), nil)
+		stateStub.GetStateReturnsOnCall(1, []byte(valKey2Value1), nil)
+		stateStub.GetStateReturnsOnCall(2, []byte(nil), nil)
+
+		// creating batch cache stub
+		batchStub := NewBatchCacheStub(stateStub)
+
+		// requesting data from state
+		result, err := batchStub.GetState(valKey1)
+		require.NoError(t, err)
+		require.Equal(t, []byte(valKey1Value1), result)
+
+		result, err = batchStub.GetState(valKey2)
+		require.NoError(t, err)
+		require.Equal(t, []byte(valKey2Value1), result)
+
+		result, err = batchStub.GetState(valKey3)
+		require.NoError(t, err)
+		require.Equal(t, []byte(nil), result)
+
+		// changing data in key 1
+		err = batchStub.PutState(valKey1, []byte(valKey1Value2))
+		require.NoError(t, err)
+
+		// deleting data in key 2
+		err = batchStub.DelState(valKey2)
+		require.NoError(t, err)
+
+		// adding and deleting data in key 3
+		err = batchStub.PutState(valKey3, []byte(valKey3Value1))
+		require.NoError(t, err)
+
+		err = batchStub.DelState(valKey3)
+		require.NoError(t, err)
+
+		// checking changed data before commit
+		result, err = batchStub.GetState(valKey1)
+		require.NoError(t, err)
+		require.Equal(t, []byte(valKey1Value2), result)
+
+		result, err = batchStub.GetState(valKey2)
+		require.NoError(t, err)
+		require.Equal(t, []byte(nil), result)
+
+		result, err = batchStub.GetState(valKey3)
+		require.NoError(t, err)
+		require.Equal(t, []byte(nil), result)
+
+		err = batchStub.Commit()
+
+		// checking data after commit
+		result, err = batchStub.GetState(valKey1)
+		require.NoError(t, err)
+		require.Equal(t, []byte(valKey1Value2), result)
+
+		result, err = batchStub.GetState(valKey2)
+		require.NoError(t, err)
+		require.Equal(t, []byte(nil), result)
+
+		result, err = batchStub.GetState(valKey3)
+		require.NoError(t, err)
+		require.Equal(t, []byte(nil), result)
+
+		// checking mock stub calls
+		require.Equal(t, 3, stateStub.GetStateCallCount())
+		require.Equal(t, 1, stateStub.PutStateCallCount())
+		require.Equal(t, 2, stateStub.DelStateCallCount())
 	})
 }
